578552d36b6b4b3c8b8bafabd5ae966e8a9c9e50
Mac User <sabbey37@gmail.com>
2017-09-24 11:51:35 -0400

    Adds header.js

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
-const App = () => {
-    return(
+const App = () => (
         <Text>Some Text</Text>
-    );
-};
+);
+
+AppRegistry.registerComponent('albums', () => App);
