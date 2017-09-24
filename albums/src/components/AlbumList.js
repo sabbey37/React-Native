63a8272f8b967e2c969229791161b334d3361b1b
Mac User <sabbey37@gmail.com>
2017-09-24 14:56:04 -0400

    Makes albumlist scroll-able

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
 // State === internal record keeping
@@ -25,9 +25,9 @@ class AlbumList extends Component {
     render() {
         console.log(this.state);
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
