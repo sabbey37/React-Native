074ba33ca19979820d4ea36eeb5b3d2d78ddb1cf
Mac User <sabbey37@gmail.com>
2017-09-24 13:34:12 -0400

    Updates state once albums retrieved from api

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -2,7 +2,18 @@ import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 
 class AlbumList extends Component {
+    state = {albums:[]};
+
+    componentWillMount() {
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        .then((response) => response.json())
+        .then((resp) => {
+            this.setState({albums: resp});
+        })
+    }
+
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album List!!!!</Text>
