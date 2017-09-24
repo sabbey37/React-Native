8aa0e8f51ff0f9b058de6734ceb5d8d340e21a79
Mac User <sabbey37@gmail.com>
2017-09-24 14:11:09 -0400

    Allows card component to render children

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <View style={styles.containerStyle}>
-            
+            {props.children}
         </View>
     );
 };
