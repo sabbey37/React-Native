bc691c8073d4e7a21109b583e13e2ec93a2faee2
Mac User <sabbey37@gmail.com>
2017-09-24 12:44:19 -0400

    Adds albumlist to App

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
+    <View>
         <Header headerText={"Albums!"}/>
+        <AlbumList/>
+    </View>
 
 );
 
