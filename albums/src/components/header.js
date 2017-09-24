cb3c0f33f5188efb32b33d39086fe944ae8636c6
Mac User <sabbey37@gmail.com>
2017-09-24 12:16:30 -0400

    Adds wrapper and styling to header

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+// View is like wrapper or DIV
+import { Text, View } from 'react-native';
 
 const Header = () => {
-    const {textStyle} = styles;
-    return <Text style={textStyle}>Albums!</Text>
+    const {textStyle, viewStyle} = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums!</Text>
+        </View>
+    );
 };
 
 const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
     textStyle: {
         fontSize: 20
     }
