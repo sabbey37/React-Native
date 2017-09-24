2c7a21ac537a3cbeefb117e271a7bb3affdc1999
Mac User <sabbey37@gmail.com>
2017-09-24 14:49:38 -0400

    Adds styling to thumbnail

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
@@ -9,16 +9,18 @@ const AlbumDetail = ({album}) => {
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={styles.thumbnailContainerStyle}>
                     <Image 
                         style={styles.thumbnailStyle} 
                         source={{uri: thumbnail_image}}
-                        />
+                    />
                 </View>
                 <View style={styles.headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={styles.headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
             </CardSection>
         </Card>
     );
@@ -29,9 +31,19 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle:  {
+        fontSize: 18
+    },
+
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 }
 
