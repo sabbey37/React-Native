83f51274016ca29ceef33c8eeaa1f3c8adb65d80
Mac User <sabbey37@gmail.com>
2017-09-24 12:03:51 -0400

    Imports header component, adds to app

---
+++ albums/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => {
+    return <Text>Albums!</Text>
+};
+
+// Make component available to other parts of application
+// Only root component uses 'AppRegistry'
+
+export default Header;
