f7bb54e85c6e91ede0fcf7381fcb0ce80ecbf659
Mac User <sabbey37@gmail.com>
2017-11-07 14:18:45 -0500

    Updates spacing

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -4,9 +4,9 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-    <View style={{flex: 1}}>
-        <Header headerText={"Albums!"}/>
-        <AlbumList/>
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums!'}/>
+        <AlbumList />
     </View>
 
 );
