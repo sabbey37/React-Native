641e62a70e23e8ad5d0c616944f6a2c5472780c9
Mac User <sabbey37@gmail.com>
2017-11-11 21:41:09 -0500

    Adds button to login form

--- auth/src/components/loginform.js
+++ auth/src/components/loginform.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Button, Card, CardSection} from './common';
 
 class LoginForm extends Component {
     render() {
         return (
-            <View />
+            <Card>
+                <CardSection />
+                <CardSection />
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
