41ba72958896b6da85a02a96f7c6db43195c288f
Mac User <sabbey37@gmail.com>
2017-11-12 18:56:14 -0500

    Exports Input component

--- auth/src/components/common/input.js
+++ auth/src/components/common/input.js
@@ -1,2 +1,12 @@
 import React, {Component} from 'react';
 import {TextInput, View, Text} from 'react-native';
+
+const Input = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+};
+
+export { Input };
