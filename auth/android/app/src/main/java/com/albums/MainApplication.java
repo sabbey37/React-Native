cb7f521e6307089d9454356366b56246d79e303f
Mac User <sabbey37@gmail.com>
2017-11-11 23:50:51 -0500

    Alters ios and android bundles

--- auth/android/app/src/main/java/com/albums/MainApplication.java
+++ auth/android/app/src/main/java/com/albums/MainApplication.java
@@ -1,4 +1,4 @@
-package com.auth;
+package com.albums;
 
 import android.app.Application;
 
@@ -24,11 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage()
       );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
     }
   };
 
--- auth/ios/albumsTests/albumsTests.m
+++ auth/ios/albumsTests/albumsTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface authTests : XCTestCase
+@interface albumsTests : XCTestCase
 
 @end
 
-@implementation authTests
+@implementation albumsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
