6d90ad2e2a6862c6aebf48c389f946033c88a3c4
Mac User <sabbey37@gmail.com>
2017-11-12 18:55:01 -0500

    Adds input.js

---
+++ auth/src/components/common/input.js
@@ -0,0 +1,2 @@
+import React, {Component} from 'react';
+import {TextInput, View, Text} from 'react-native';
--- auth/src/components/loginform.js
+++ auth/src/components/loginform.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {TextInput} from 'react-native';
-import {Button, Card, CardSection} from './common';
+import {Button, Card, CardSection, Input} from './common';
 
 class LoginForm extends Component {
     state = {text: ''};
