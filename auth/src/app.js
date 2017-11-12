9b3def9a4ea37bfbe3853186d2a1a703848278df
Mac User <sabbey37@gmail.com>
2017-11-11 21:34:12 -0500

    Sets up firebase

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAnTfbr_gV6r3IMFvl9qHrGU-J9b_OpFIc",
+            authDomain: "auth-cf0ba.firebaseapp.com",
+            databaseURL: "https://auth-cf0ba.firebaseio.com",
+            projectId: "auth-cf0ba",
+            storageBucket: "auth-cf0ba.appspot.com",
+            messagingSenderId: "139029549482"
+        })
+    }
+
     render() {
         return (
             <View>
