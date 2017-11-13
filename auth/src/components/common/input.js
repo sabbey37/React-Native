d73e0bd3edfcf26338d9450db2fe67db7f3a7a78
Mac User <sabbey37@gmail.com>
2017-11-13 13:31:54 -0500

    Adds style to text input

--- auth/src/components/common/input.js
+++ auth/src/components/common/input.js
@@ -14,4 +14,21 @@ const Input = ({label, value, onChangeText}) => {
     )
 };
 
+const styles = {
+    inputStyle: {
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    containerStyle: {}
+};
+
 export { Input };
