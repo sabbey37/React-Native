b72bfc8e6d394d6c16a4f23c79c904bdc90c9e5d
Mac User <sabbey37@gmail.com>
2017-11-21 17:37:00 -0500

    Puts native ActivityIndicator inside of View

--- auth/src/components/common/Spinner.js
+++ auth/src/components/common/Spinner.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 
 const Spinner = () => {
     return (
-        <View />
+        <View>
+            <ActivityIndicator />
+        </View>
     );
 }
 
