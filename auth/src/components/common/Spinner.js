5e88969e28c2b7f28a81ad796d91da7696c20712
Mac User <sabbey37@gmail.com>
2017-11-21 18:54:47 -0500

    Adds styling to spinner

--- auth/src/components/common/Spinner.js
+++ auth/src/components/common/Spinner.js
@@ -3,10 +3,18 @@ import {View, ActivityIndicator} from 'react-native';
 
 const Spinner = () => {
     return (
-        <View>
+        <View style={styles.spinnerStyle}>
             <ActivityIndicator />
         </View>
     );
+};
+
+const styles = {
+    spinnerStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 }
 
 export {Spinner};
