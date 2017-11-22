d187740cde0ff2363c59cafca5926930db056991
Mac User <sabbey37@gmail.com>
2017-11-22 16:44:17 -0500

    Calls reducers with store

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import reducers from './reducers';
 
 const App = () => {
     return (
-        <Provider store={createStore}>
+        <Provider store={createStore(reducers)}>
             <View />
         </Provider>
     );
