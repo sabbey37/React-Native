77d3639397fdcd6aa22639abdfce9ce55bab72e6
Mac User <sabbey37@gmail.com>
2017-11-21 19:11:51 -0500

    Updates error and loading status on login fail

--- auth/src/components/loginform.js
+++ auth/src/components/loginform.js
@@ -15,15 +15,19 @@ class LoginForm extends Component {
         const { email, password } = this.state;
         this.setState({error: '', loading: true});
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(() => {
-
-            })
+            .then(this.onLoginSuccess.bind(this))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .catch(() => {
-                        this.setState({error: "Authentication Failed."});
-                    });
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
             });
+    }
+
+    onLoginFail() {
+        this.setState({
+            error: "Authentication Failed.",
+            loading: false
+        })
     }
 
     onLoginSuccess() {
