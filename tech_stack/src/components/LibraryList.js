1b466fd9f342ca822a77d3daa3b2e8e463c55c5d
Mac User <sabbey37@gmail.com>
2017-11-24 13:43:11 -0500

    Uses connect from react-redux library

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class LibraryList extends Component {
     render() {
@@ -6,4 +7,4 @@ class LibraryList extends Component {
     }
 }
 
-export default LibraryList;
+export default connect()(LibraryList);
