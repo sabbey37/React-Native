cd21ac5c8776b54045649a09e391e622a308a12a
Mac User <sabbey37@gmail.com>
2017-11-30 11:42:27 -0500

    Creates ListView in LibraryList component

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import {ListView} from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2 
+        });
+    }
+
     render() {
         return;
     }
