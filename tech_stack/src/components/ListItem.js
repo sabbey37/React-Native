763006cf7213d0f79c708fe0689e110a01cd16a2
Mac User <sabbey37@gmail.com>
2017-11-30 12:03:37 -0500

    Imports listitem

---
+++ tech_stack/src/components/ListItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {ListView} from 'react-native';
+import { connect } from 'react-redux';
+
+class ListItem extends Component {
+    render() {
+        return;
+    }
+}
+
+export default ListItem;
