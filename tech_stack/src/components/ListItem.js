288baee905c9892ea22d9143762671017127cf12
Mac User <sabbey37@gmail.com>
2017-12-03 17:59:40 -0500

    Adds Touchable component

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableWithoutFeedback, View} from 'react-native';
 import {connect} from 'react-redux';
 import {CardSection} from './common';
 import * as actions from '../actions';
@@ -8,11 +8,15 @@ class ListItem extends Component {
     render() {
         const {titleStyle} = styles;
         return (
-            <CardSection>
-                <Text style={titleStyle}>
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback>
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {this.props.library.title}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
