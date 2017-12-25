885543002a3c031bef326057d2ed5c93b22b626a
Mac User <sabbey37@gmail.com>
2017-12-25 13:38:50 -0500

    Adds firebase config to app

--- manager/src/App.js
+++ manager/src/App.js
@@ -3,8 +3,21 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        // Initialize Firebase
+        const config = {
+            apiKey: "AIzaSyB-sjS4GdGGUhDSgOYyERZ-P4igrJwS2CA",
+            authDomain: "manager-67d3e.firebaseapp.com",
+            databaseURL: "https://manager-67d3e.firebaseio.com",
+            projectId: "manager-67d3e",
+            storageBucket: "",
+            messagingSenderId: "18225082598"
+        };
+        firebase.initializeApp(config);
+    }
     render() {
         return (
             <Provider store={createStore(reducers)}>
