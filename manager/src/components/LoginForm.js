d27a6cbc6490e73d8e9550fb8e20b4f17ebf391f
Mac User <sabbey37@gmail.com>
2017-12-27 19:11:25 -0500

    Creates LoginForm component

---
+++ manager/src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React, { Componenet } from 'react';
+import { Card } from './common';
+
+class LoginForm extends Componenet {
+    render() {
+        return (
+            <Card>
+
+            </Card>
+        );
+    }
+}
+
+export default LoginForm;
